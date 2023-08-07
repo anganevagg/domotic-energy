a08caca90a78fba49667cbd324571a41bfe63542
Anganevagg <anganevagg@protonmail.com>
2023-08-07 08:15:06 -0600

    first revision

---
+++ components/ui/header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="Cruip">
+              <img src="/logo.jpeg" alt="" width={150}/>
+            </Link>
+          </div>
+
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
